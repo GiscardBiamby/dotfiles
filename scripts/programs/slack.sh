2f91458814a33fdc47a2b9b9384981902064509c
Giscard Biamby <giscard@gmail.com>
2021-09-05 14:45:26 -0700

    Change slack install to use .deb instead of snap package

--- scripts/programs/slack.sh
+++ scripts/programs/slack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo 'Install Slack'
-sudo snap install slack --classic
+# sudo snap install slack --classic
+wget https://downloads.slack-edge.com/releases/linux/4.19.2/prod/x64/slack-desktop-4.19.2-amd64.deb
+dpkg -i slack-desktop*.deb
+sudo apt-get update
+sudo apt-get upgrade slack-desktop
