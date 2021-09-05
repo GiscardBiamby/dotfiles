50865ddac274d6b363498aba1ef76e4e050a92a6
Giscard Biamby <giscard@gmail.com>
2021-09-05 14:14:54 -0700

    Update postman install script to scripted install instead of snap install

---
+++ scripts/programs/postman.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. util.sh
+
+echo "Installing postman"
+# https://www.how2shout.com/linux/how-to-install-postman-on-ubuntu-20-04-lts-linux/
+wget https://dl.pstmn.io/download/latest/linux64
+sudo tar -xvf linux64 -C /usr/bin
+
+# TODO: Add to .bashrc
+export PATH=$PATH:/usr/bin/Postman
+
+# Setup desktop shortcut
+{
+    echo "[Desktop Entry]"
+    echo "Name=Postman API Tool"
+    echo "GenericName=Postman"
+    echo "Comment=Testing API"
+    echo "Exec=/usr/bin/Postman/Postman"
+    echo "Terminal=false"
+    echo "X-MultipleArgs=false"
+    echo "Type=Application"
+    echo "Icon=/usr/bin/Postman/app/resources/app/assets/icon.png"
+    echo "StartupWMClass=Postman"
+    echo "StartupNotify=true"
+} >> /usr/share/applications/Postman.desktop
+
+# Uninstall
+# sudo rm -r /usr/bin/Postman
+# sudo rm -r /usr/share/applications/Postman.desktop
+# sudo rm -r ~/Desktop/Postman.desktop
