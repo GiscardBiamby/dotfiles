b1b42f1e5d66a38b34558ebe05f371b4c4df2591
Giscard Biamby <giscard@gmail.com>
2021-09-05 14:37:56 -0700

    Implement spotify install script

---
+++ scripts/programs/spotify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. util.sh
+
+echo "Installing Spotify"
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+install spotify-client
