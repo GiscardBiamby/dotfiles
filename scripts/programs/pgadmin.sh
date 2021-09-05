c9d41d25f72fb92d789b01c254f6585d5037c869
Giscard Biamby <giscard@gmail.com>
2021-09-05 14:31:08 -0700

    Add note to pgadmin4 install

--- scripts/programs/pgadmin.sh
+++ scripts/programs/pgadmin.sh
@@ -3,8 +3,10 @@
 . util.sh
 
 echo "Installing pgadmin"
+# https://www.pgadmin.org/download/pgadmin-4-apt/
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 
 # Install for desktop mode only:
 sudo apt install pgadmin4-desktop
+echo "You can copy ~/.pgadmin/pgadmin4.db between machines to transfer server definitions"
