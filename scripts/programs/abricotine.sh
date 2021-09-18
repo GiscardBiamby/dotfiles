b1f3e18279e3569ca26b806397da6a1a104f00b6
Giscard Biamby <giscard@gmail.com>
2021-09-17 17:23:15 -0700

    Add folder to put program downloads into

--- scripts/programs/abricotine.sh
+++ scripts/programs/abricotine.sh
@@ -5,5 +5,5 @@
 echo " Installing Abricotine"
 VERSION=$(curl -s https://github.com/brrd/Abricotine/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
 echo "Abricotine version: ${VERSION}"
-wget "https://github.com/brrd/Abricotine/releases/latest/download/abricotine-${VERSION}-ubuntu-debian-x64.deb"
+wget "https://github.com/brrd/Abricotine/releases/latest/download/abricotine-${VERSION}-ubuntu-debian-x64.deb" 
 sudo dpkg -i abricotine*.deb
