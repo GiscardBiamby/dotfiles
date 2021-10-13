324509846c4f980960acb559aae87ebb93f5c985
Giscard Biamby <giscard@gmail.com>
2021-10-12 22:34:21 -0700

    Add conda-bash-completion to anaconda.sh

--- scripts/programs/anaconda.sh
+++ scripts/programs/anaconda.sh
@@ -7,12 +7,12 @@ echo "🐍 Installing anaconda"
 if [[ ! -f "anaconda3_x86_64.sh" ]]; then
     wget -O - https://www.anaconda.com/distribution/ 2>/dev/null |
         sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' |
-        xargs wget -O anaconda3_x86_64.sh
+        xargs wget -O downloads/anaconda3_x86_64.sh
 fi
 
 # Silent install (note: it doesn't add the PATH stuff to .bashrc, so make sure to add
 # that into the .bashrc in this dotfiles repo):
-bash ./anaconda3_x86_64.sh -b \
+bash ./downloads/anaconda3_x86_64.sh -b \
     -p ~/anaconda3
 
 # activate in current shell session:
@@ -21,5 +21,6 @@ eval "$(~/anaconda3/bin/conda shell.bash hook)"
 # Install conda's shell functions:
 conda init
 conda update conda
+conda install -c conda-forge conda-bash-completion
 
-rm ./anaconda3_x86_64.sh
+rm ./downloads/anaconda3_x86_64.sh
