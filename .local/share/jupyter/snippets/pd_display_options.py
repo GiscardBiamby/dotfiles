5bbeda63826afe35447e9d502446ea4f9e81a82f
Giscard Biamby <giscard@gmail.com>
2022-04-26 04:59:31 +0000

    Update jupyter snippets

--- .local/share/jupyter/snippets/pd_display_options.py
+++ .local/share/jupyter/snippets/pd_display_options.py
@@ -3,7 +3,7 @@ pd.set_option("display.max_columns", 15)
 pd.set_option("display.max_rows", 50)
 # Suitable default display for floats
 pd.options.display.float_format = "{:,.2f}".format
-plt.rcParams["figure.figsize"] = (12, 10)
+plt.rcParams["figure.figsize"] = (20, 10)
 
 # This one is optional -- change graphs to SVG only use if you don't have a
 # lot of points/lines in your graphs. Can also just use ['retina'] if you
--- .local/share/jupyter/snippets/plt_display_options.py
+++ .local/share/jupyter/snippets/plt_display_options.py
@@ -1,16 +1,24 @@
 set_matplotlib_formats('pdf', 'png')
-plt.rcParams['savefig.dpi'] = 75
+# plt.rcParams['savefig.dpi'] = 75
 
 plt.rcParams['figure.autolayout'] = False
-plt.rcParams['figure.figsize'] = 10, 6
 plt.rcParams['axes.labelsize'] = 18
 plt.rcParams['axes.titlesize'] = 20
 plt.rcParams['font.size'] = 16
 plt.rcParams['lines.linewidth'] = 2.0
 plt.rcParams['lines.markersize'] = 8
 plt.rcParams['legend.fontsize'] = 14
-plt.rcParams['text.usetex'] = True
 
-plt.rcParams['font.family'] = "serif"
-plt.rcParams['font.serif'] = "cm"
+# plt.rcParams['text.usetex'] = True
+# plt.rcParams['font.family'] = "serif"
+# plt.rcParams['font.serif'] = "cm"
 # plt.rcParams['text.latex.preamble'] = "\\usepackage{subdepth}, \\usepackage{type1cm}"
+
+sns.set_style("dark")
+sns.set(
+    rc={
+        "figure.figsize": (20, 10),
+        "axes.grid": True,
+    },
+    palette="colorblind",
+)
