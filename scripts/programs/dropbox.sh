a3b413dcec1f6de997703d5189baab9911b9ebae
Giscard Biamby <giscard@gmail.com>
2022-09-06 22:45:55 -0700

    Add comment

--- scripts/programs/dropbox.sh
+++ scripts/programs/dropbox.sh
@@ -4,5 +4,7 @@
 
 echo "Installing dropbox"
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-# This might freeze up the scripts, not sure how to best handle it:
+
+# Nohup to launch GUI app detached (aka. "disowned", "disentangled", "decoupled") (from:
+# https://askubuntu.com/a/46211):
 nohup ~/.dropbox-dist/dropboxd &
