88a1c706d030df226dff48bd9d3ad294ca55f184
Giscard Biamby <giscard@gmail.com>
2023-06-19 17:11:36 -0700

    Remove -y from mamba install command (I assume it was causing a problem, can't remember why I did this)

--- scripts/programs/mamba.sh
+++ scripts/programs/mamba.sh
@@ -8,7 +8,7 @@ echo "🐍 Installing mamba"
 # Latest installers with Mamba in the base environment:
 mkdir -p ./programs/downloads/micromamba
 wget -O "./programs/downloads/micromamba/Mambaforge-$(uname)-$(uname -m).sh" "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash ./programs/downloads/micromamba/Mambaforge-$(uname)-$(uname -m).sh -y
+bash ./programs/downloads/micromamba/Mambaforge-$(uname)-$(uname -m).sh
 
 
 
