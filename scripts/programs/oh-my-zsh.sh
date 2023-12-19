3695984dead7994db7e6a1534a9a5b5983e7af15
Giscard Biamby <giscard@gmail.com>
2023-12-19 07:41:48 +0000

    add conda-zsh-completion to oh-my-zsh.sh

--- scripts/programs/oh-my-zsh.sh
+++ scripts/programs/oh-my-zsh.sh
@@ -16,3 +16,4 @@ fi
 # Install zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/esc/conda-zsh-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/conda-zsh-completion
