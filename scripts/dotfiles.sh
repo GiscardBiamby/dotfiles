25269859ccb0f56894eba8b78d6ae63450b81dd5
Giscard Biamby <giscard@gmail.com>
2024-01-27 17:01:46 -0800

    - Add basic .vimrc - Make `scripts/dotfiles.sh` able to run from any location

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,43 +3,46 @@
 # Get the directory of this script so that we can reference paths correctly no matter which folder
 # the script was launched from:
 SCRIPT_DIR="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJ_ROOT="${SCRIPT_DIR}/../"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 source "${SCRIPT_DIR}/util.sh"
 
-
+pushd "${SCRIPT_DIR}"
 python backup_dotfiles.py
+popd
 
-pushd ../stow_packages
+pushd "${PROJ_ROOT}/stow_packages"
+stow --target="${HOME}" ag
 stow --target="${HOME}" bash
 stow --target="${HOME}" conda
 stow --target="${HOME}" direnv
 stow --target="${HOME}" git
 stow --target="${HOME}" kitty
 stow --target="${HOME}" tmux
-stow --target="${HOME}" ag
+stow --target="${HOME}" vim
 stow --target="${HOME}" zsh_linux
 popd
 
 # Manage vscode settings and keybindings if vscode is installed:
-pushd ../.config/Code
+pushd "${PROJ_ROOT}/.config/Code"
 if [[ -d "${HOME}/.config/Code/User" ]]; then
     stow --target="${HOME}/.config/Code/User" User
 fi
 popd
 
-pushd ../.config
+pushd "${PROJ_ROOT}/.config"
 if [[ -d "${HOME}/.config/Code" ]]; then
     stow --target="${HOME}/.config/Code" Code
 fi
 popd
 
-pushd ../.local/share
+pushd "${PROJ_ROOT}/.local/share"
 if [[ -d "${HOME}/.local/share/jupyter" ]]; then
     stow --target="${HOME}/.local/share/jupyter" jupyter
 fi
 popd
 
-pushd ../.jupyter
+pushd "${PROJ_ROOT}/.jupyter"
 if [[ -d "${HOME}/.jupyter" ]]; then
     stow --target="${HOME}/.jupyter/nbconfig" nbconfig
 fi
