87b56d06e2fa682d2762dd096b2ddac566353a35
Giscard Biamby <giscard@gmail.com>
2024-03-10 08:19:24 -0700

    Make direnv install use latest release (don't hardcode version in download url)

--- scripts/programs/direnv.sh
+++ scripts/programs/direnv.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-. ../util.sh
+# Get the directory of this script so that we can reference paths correctly no matter which folder
+# the script was launched from:
+SCRIPT_DIR="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+source "${SCRIPT_DIR}/../util.sh"
 
 echo "📂 Installing direnv"
 
-curl -qfsSL https://github.com/direnv/direnv/releases/download/v2.19.0/direnv.linux-amd64 -o direnv
-chmod +x direnv
-sudo mv direnv /usr/local/bin
+# curl -qfsSL https://github.com/direnv/direnv/releases/download/v2.19.0/direnv.linux-amd64 -o direnv
+wget \
+    https://github.com/direnv/direnv/releases/latest/download/direnv.linux-amd64 \
+    -O "${SCRIPT_DIR}/downloads/direnv"
+chmod +x "${SCRIPT_DIR}/downloads/direnv"
+sudo mv "${SCRIPT_DIR}/downloads/direnv" /usr/local/bin
