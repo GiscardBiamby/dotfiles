ae7da3bae15dc5df4aaaa4a25f63474fcacbd3dc
Giscard Biamby <giscard@gmail.com>
2025-11-24 07:48:20 -0800

    Add comments to keepassxc.sh

--- scripts/programs/keepassxc.sh
+++ scripts/programs/keepassxc.sh
@@ -7,8 +7,11 @@ echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}/../util.sh"
 
-
 echo "🔐 Installing keepassxc"
+# ppa:phoerious/keepassxc is the ppa for the latest keepassxc releases mentioned on the official
+# keepassxc website. Note about other keepass packages: keepassx is dead, keepass2 is the original
+# windows app running on linux via mono/wine, poor wayland integration and often requires xwayland.
+# keepassxc is the modern active community-driven fork. Native linux (qt-based, and wayland-native).
 sudo add-apt-repository -y ppa:phoerious/keepassxc
 sudo apt update
 install keepassxc
